d91781f3857a264a378491e9deb0478e02a3e6be
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-07 18:35:05 +0200

    added main

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,10 +1,21 @@
 package core.basesyntax;
 
+import core.basesyntax.controller.BetHandler;
+import core.basesyntax.controller.ConsoleHandler;
+import core.basesyntax.controller.UserHandler;
+
 /**
  * Feel free to remove this class and create your own.
  */
 public class Main {
     public static void main(String[] args) {
+        ConsoleHandler betHandler = new BetHandler();
+        ConsoleHandler userHandler = new UserHandler();
     
+        System.out.println("Give a bet and risk, please");
+        betHandler.handle();
+    
+        System.out.println("Give a first and lastName, please");
+        userHandler.handle();
     }
 }
