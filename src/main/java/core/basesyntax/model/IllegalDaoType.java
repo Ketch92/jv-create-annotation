b1fc1fc50df4bfd0859832d903ae4bd42ee9cf04
Oleh Kravets <oleg.kravets92@gmail.com>
2021-01-09 19:06:47 +0200

    Small refactoring

--- src/main/java/core/basesyntax/model/IllegalDaoType.java
+++ src/main/java/core/basesyntax/model/IllegalDaoType.java
@@ -1,8 +1,9 @@
 package core.basesyntax.model;
 
 public class IllegalDaoType extends RuntimeException {
+    private static final String MESSAGE = "Not correct dao implementation";
     
     public IllegalDaoType() {
-        super("Not correct dao implementation");
+        super(MESSAGE);
     }
 }
